6d27cf563772e8971ee9889ed32dbb2a3aad56e5
Gus <gustavo@canya.com>
2018-06-29 07:47:50 -0600

    Removes barrels from public_api.ts

--- projects/common-lib/src/lib/footer/footer.component.ts
+++ projects/common-lib/src/lib/footer/footer.component.ts
@@ -8,9 +8,8 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
--- projects/common-lib/src/public_api.ts
+++ projects/common-lib/src/public_api.ts
@@ -2,13 +2,11 @@
  * Public API Surface of common-lib
  */
 
-export * from './lib/@model/language.model';
-export * from './lib/@model/nav-item.model';
-export * from './lib/@service/language.service';
-export * from './lib/@pipe/keep-html.pipe';
-
-export * from './lib/header/header.component';
-export * from './lib/footer/footer.component';
-export * from './lib/language-switch/language-switch.component';
-
-export * from './lib/common-lib.module';
+export { Language } from './lib/@model/language.model';
+export { NavItem } from './lib/@model/nav-item.model';
+export { LanguageService } from './lib/@service/language.service';
+export { KeepHtmlPipe } from './lib/@pipe/keep-html.pipe';
+export { HeaderComponent } from './lib/header/header.component';
+export { FooterComponent } from './lib/footer/footer.component';
+export { LanguageSwitchComponent } from './lib/language-switch/language-switch.component';
+export { CommonLibModule } from './lib/common-lib.module';
